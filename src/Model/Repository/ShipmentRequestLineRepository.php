21842694486ae81528f1a0eed1256c42e149ecbd
Marc Espinosa <marespinosa@ebay.com>
2016-07-12 18:39:24 +0200

    Adding missing repository

---
+++ src/Model/Repository/ShipmentRequestLineRepository.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Bootstrap\Model\Repository;
+
+class ShipmentRequestLineRepository extends AbstractRepository
+{
+}
